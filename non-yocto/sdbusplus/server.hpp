b654e8a28a92039b84ff3acea87306cbc747c871
Jia, Chunhui <chunhui.jia@intel.com>
2018-08-17 14:44:52 +0800

    Intel OEM commands
    Intel platform defines a set of OEM commands. They are required by
    Intel BIOS.
    
    This change adds 5 OEM commands:
    1.get chassis identifier
    Return chassis serial number
    
    2.set system GUID
    Sets the system GUID obtained according to the format of the IPMI
    2.0 Get System GUID command. While the setting is persistent in the
    BMC, the BIOS may choose to over-write the value during POST.
    
    3.set BIOS ID
    BIOS version string
    
    4.get device info
    BMC/HSC/ME version
    
    5.get AIC(add-in-card) FRU
    Add-in-card FRU info
    
    Tested: Test with ipmitool raw command on Intel Wolfpass
    Platform. BIOS calling is also verified by checking ipmi handler
    log (journalctl -ef).
    
    Change-Id: I658c5845cd1252f55dc5d24dfc142281b6a38363
    Signed-off-by: Jia, Chunhui <chunhui.jia@linux.intel.com>

---
+++ non-yocto/sdbusplus/server.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/bus/match.hpp>
+// Alias for deprecated sdbusplus::server::match namespace.
+namespace sdbusplus
+{
+namespace server
+{
+namespace match = bus::match;
+using match_t = bus::match_t;
+} // server
+} // sdbusplus
+#include <sdbusplus/vtable.hpp>
+
+#include <sdbusplus/server/interface.hpp>
+#include <sdbusplus/server/manager.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <sdbusplus/server/bindings.hpp>
+#if 1
+#include <sdbusplus/server/transaction.hpp>
+#endif
