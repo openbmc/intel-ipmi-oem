a2a876dcae39093f46023a873b80ca7e814d09f9
ssekar <suryakanth.sekar@linux.intel.com>
2018-11-28 15:47:22 +0530

    Adding the OOB related OEM comamnds - First Phase
    GetPayload command,GetBIOSHash, Set BIOSHashinfo
    GetOOB capabilites and SetOOB Capabilites are implemented
    
    Change-Id: I844170dcaea1259ad1130caa83410850317acd06
    Signed-off-by: ssekar <suryakanth.sekar@linux.intel.com>

--- include/oobcommands.hpp
+++ include/oobcommands.hpp
@@ -18,37 +18,51 @@
 
 #pragma pack(push, 1)
 
-enum class OOBImageType {
+static constexpr uint8_t maxHashSize = 32;
+static constexpr uint8_t maxSeedSize = 16;
+static constexpr uint8_t maxPasswordSize = 32;
+static constexpr uint32_t maxPayloadLength = (64 * 1024 * 1024); // 64MB
+static constexpr uint32_t maxPayloadLengthPerTransfer = 65512;
+
+enum OOBImageType
+{
     BIOSXMLType0 = 0,
     BIOSXMLType1 = 1,
-    BIOSImage    = 2,
-    MEImage      = 3,
-    FDImage      = 4,
-    invalidType,  //this item should always be the last one
+    BIOSImage = 2,
+    MEImage = 3,
+    FDImage = 4,
+    invalidType, // this item should always be the last one
 };
 
 struct SetPayloadReq
 {
-    uint8_t paraSel;      //Parameter Selector  0-Init 1-in Progress 2 -End of  transfer 3 - user abort
-    OOBImageType payloadType;  // 1 byte	 0-XML type 0 ; 1-XML type 1; 2-BIOS; 3 - ME image; 4- FD image
-    union Input{
+    uint8_t paraSel; // Parameter Selector  0-Init 1-in Progress 2 -End of
+                     // transfer 3 - user abort
+    OOBImageType payloadType; // 1 byte	 0-XML type 0 ; 1-XML type 1; 2-BIOS; 3
+                              // - ME image; 4- FD image
+    union Input
+    {
         struct Parameter0
         {
-            uint16_t payloadVersion;  // 2 bytes
-            uint32_t payloadSize;     // 4 bytes
-            uint32_t payloadChecksum; // 4 bytes
-            uint8_t  payloadFlag;      // 1 byte
-            uint8_t  BIOSPassword[maxPasswordLength]; // 32 bytes  (only available when PayloadType == 1)
-        } Para0;      //when ParaSel == 0
+            uint16_t payloadVersion;               // 2 bytes
+            uint32_t payloadSize;                  // 4 bytes
+            uint32_t payloadChecksum;              // 4 bytes
+            uint8_t payloadFlag;                   // 1 byte
+            uint8_t BIOSPassword[maxPasswordSize]; // 32 bytes  (only available
+                                                   // when PayloadType == 1)
+        } Para0;                                   // when ParaSel == 0
 
         struct Parameter1
         {
             uint32_t reservationToken;
-            uint32_t writingPayloadOffset;   // 4 bytes Writing payload data length
-            uint32_t writingPayloadLength;   // 4 bytes Writing payload data length
-            uint32_t writingPayloadChecksum; // 4 bytes Writing payload data checksum
-            uint8_t  data[maxPayloadLengthPerTransfer];                  // data pointer
-        } Para1;      //when ParaSel == 1
+            uint32_t
+                writingPayloadOffset; // 4 bytes Writing payload data length
+            uint32_t
+                writingPayloadLength; // 4 bytes Writing payload data length
+            uint32_t
+                writingPayloadChecksum; // 4 bytes Writing payload data checksum
+            uint8_t data[maxPayloadLengthPerTransfer]; // data pointer
+        } Para1;                                       // when ParaSel == 1
         struct Parameter2
         {
             uint32_t reservationToken;
@@ -57,60 +71,157 @@ struct SetPayloadReq
         {
             uint32_t reservationToken;
         } Para3;
-
-    } ;
-} ;
-
+    };
+};
 
 union SetPayloadResp
 {
-    uint32_t reservationToken;           //paramter == 0
-    uint32_t actualPayloadWritten;       //paramter == 1           in progress
-    uint32_t actualTotalPayloadWritten;  //paramter == 2  or 3     end of transfer or user abort
-} ;
+    uint32_t reservationToken;          // paramter == 0
+    uint32_t actualPayloadWritten;      // paramter == 1           in progress
+    uint32_t actualTotalPayloadWritten; // paramter == 2  or 3     end of
+                                        // transfer or user abort
+};
 
+struct GetPayloadReq
+{
+    uint8_t paraSel; // Parameter Selector  0-Init 1-in Progress 2 -End of
+                     // transfer 3 - user abort
+    union Input
+    {
+        struct Parameter0
+        {
+            uint8_t payloadType; // 1 byte    0-XML type 0 ; 1-XML type 1;
+                                 // 2-BIOS; 3 - ME image; 4- FD image
+        } Para0;                 // when ParaSel == 0
+
+        struct Parameter1
+        {
+
+            uint8_t payloadType;
+            uint32_t
+                payloadOffset; // 4 bytes Writing payload data offset to read
+            uint32_t payloadLength; // 4 bytes  payload data length
+        } Para1;                    // when ParaSel == 1
+        struct Parameter2
+        {
+            uint8_t payloadType; // 1 byte    0-XML type 0 ; 1-XML type 1;
+                                 // 2-BIOS; 3 - ME image; 4- FD image
+        } Para2;                 // when ParaSel == 2
+
+    } Input;
+};
+
+struct GetPayloadResp
+{
+    union Output
+    {
+        struct Parameter0
+        {
+            uint16_t payloadVersion; // 1 byte    0-XML type 0 ; 1-XML type 1;
+                                     // 2-BIOS; 3 - ME image; 4- FD image
+            uint8_t payloadType;
+            uint32_t totalPayloadlength;
+            uint32_t totalPayloadchecksum;
+            uint8_t payloadCurrentStatus;
+            uint8_t payloadFlag;
+            uint32_t timeStamp;
+        } Para0; // when ParaSel == 0
+
+        struct Parameter1
+        {
+            uint8_t payloadType;
+            uint32_t actualPayloadlength; // 4 bytes  payload data length
+            uint32_t
+                actualPayloadChecksum; // 4 bytes Writing payload data checksum
+            uint8_t payloadData[maxPayloadLengthPerTransfer];
+        } Para1; // when ParaSel == 1
+        struct Parameter2
+        {
+            uint8_t status;
+        } Para2; // when ParaSel == 2
+
+    } Output;
+};
 
 struct DataTransfer
 {
-    uint8_t  imageType;
-    uint8_t  status;
-    uint8_t  payloadFlag;
-    uint16_t payloadVersion;        //used by XML payload to indicate change order
+    uint8_t imageType;
+    uint8_t status;
+    uint8_t payloadFlag;
+    uint16_t payloadVersion; // used by XML payload to indicate change order
     uint32_t givenPayloadSize;
     uint32_t givenPayloadChecksum;
     uint32_t actualPayloadSize;
     uint32_t actualPayloadChecksum;
     uint32_t uploadTimeStamp;
-    int    fp;                   // fd for image data
+    int fp; // fd for image data
     uint32_t reservationToken;
-    union ExData {
-        uint8_t  BIOSPassword[maxPasswordLength];
-    } ;
+    union ExData
+    {
+        uint8_t BIOSPassword[maxPasswordSize];
+    };
 };
 
-
-
-enum OOBCompleteCode {
+enum OOBCompleteCode
+{
     compcodePayloadTypeNotSupported = 0x80,
-    compcodePayloadChecksumFailed   = 0x81,
-    compcodeCmdNotSupportedInState  = 0x82,
-    compcodeTransferIncomplete      = 0x83,
-    compcodePayloadPktMissing       = 0x84,
-    compcodePayloadLengthIllegal    = 0x85,
-    compcodePwdVerficationFailed    = 0x86,
-    compcodeOSError                 = 0x87,
-    compcodePayloadExeceedMaximum   = 0x88,
-    compcodeNoSpaceForPayload       = 0xC4,
+    compcodePayloadChecksumFailed = 0x81,
+    compcodeCmdNotSupportedInState = 0x82,
+    compcodeTransferIncomplete = 0x83,
+    compcodePayloadPktMissing = 0x84,
+    compcodePayloadLengthIllegal = 0x85,
+    compcodePwdVerficationFailed = 0x86,
+    compcodeOSError = 0x87,
+    compcodePayloadExeceedMaximum = 0x88,
+    compcodeNoSpaceForPayload = 0xC4,
 };
 
-enum TransferState{
+enum TransferState
+{
     StartTransfer = 0,
-    InProgress    = 1,   
+    InProgress = 1,
     EndOfTransfer = 2,
-    UserAbort     = 3,
-    NoImagePending= 4,
-    PasswdAuth    = 5,
+    UserAbort = 3,
+    NoImagePending = 4,
+    PasswdAuth = 5,
 };
 
+enum GetPayloadParameter
+{
+    PayloadInfo = 0,
+    PayloadData = 1, // data transfer
+    PayloadStatus = 2
+};
+
+struct setBIOSCapabilitiesReq
+{
+    uint8_t OOBCapability;
+    uint8_t reserved1;
+    uint8_t reserved2;
+    uint8_t reserved3;
+};
+
+struct getBIOSCapabilitiesResp
+{
+
+    uint8_t OOBCapability;
+    uint8_t reserved1;
+    uint8_t reserved2;
+    uint8_t reserved3;
+};
+
+struct setBIOSHashInfoReq
+{
+
+    uint8_t BIOSPwdHash[maxHashSize];
+    uint8_t BIOSPwdSeed[maxSeedSize];
+    uint8_t hashAlgo;
+};
+
+struct getUserBIOSHashResp
+{
+
+    uint8_t userPwdHash[maxHashSize];
+};
 
 #pragma pack(pop)
--- src/oobcommands.cpp
+++ src/oobcommands.cpp
@@ -16,47 +16,100 @@
 
 #include "xyz/openbmc_project/Common/error.hpp"
 #include <host-ipmid/ipmid-api.h>
+#include <ImageUpdate.hpp>
 #include <array>
 #include <commandutils.hpp>
 #include <iostream>
 #include <oemcommands.hpp>
+#include <oobcommands.hpp>
 #include <phosphor-ipmi-host/utils.hpp>
 #include <phosphor-logging/log.hpp>
 #include <sdbusplus/bus.hpp>
-#include <oobcommands.hpp>
 
 namespace ipmi
 {
 static void initOOB() __attribute__((constructor));
 sdbusplus::bus::bus dbus(ipmid_get_sd_bus_connection()); // from ipmid-api.h
 
-static constexpr uint8_t imageTypes = 5; //0-XML type 0 / 1- xml type 1 / 2-bios image / 3- ME image / 4 -allinone (BIOS and ME) image
+static constexpr uint8_t imageTypes = 5;
+
+static setBIOSCapabilitiesReq mBIOSCapabilities;
+static uint8_t mIsBIOSCapInitDone = false;
+static setBIOSHashInfoReq mBIOSHashInfo;
+static uint8_t mUserPwdHash[maxHashSize] = {0};
+static uint8_t IsUserPwdInitDone = false;
 
 std::vector<DataTransfer> gPayload;
 
-ipmi_ret_t ipmiOOBSetPayload(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
-                             ipmi_request_t request,
-                             ipmi_response_t response,
-                             ipmi_data_len_t dataLen,
-                             ipmi_context_t context)
+ipmi_ret_t ipmiOOBSetBIOSCap(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
+                             ipmi_request_t request, ipmi_response_t response,
+                             ipmi_data_len_t dataLen, ipmi_context_t context)
 {
-    SetPayloadReq*  pPayloadReq  = reinterpret_cast<SetPayloadReq*>(request);
-    SetPayloadResp* pPayloadResp = reinterpret_cast<SetPayloadResp*>(response);
-    uint32_t u32Crc;
-    size_t   returnSize=0;
-    OOBImageType currentPayloadType = pPayloadReq->payloadType;
-    ipmi_ret_t rc=IPMI_CC_OK;
+    ipmi_ret_t rc = IPMI_CC_OK;
+    *dataLen = 0;
+    setBIOSCapabilitiesReq *setBIOSCap =
+        reinterpret_cast<setBIOSCapabilitiesReq *>(request);
+    if (setBIOSCap->reserved1 != 0 || setBIOSCap->reserved2 != 0 ||
+        setBIOSCap->reserved3 != 0)
+    {
+        rc = IPMI_CC_INVALID_FIELD_REQUEST;
+        return rc;
+    }
+    mBIOSCapabilities.OOBCapability = setBIOSCap->OOBCapability;
+    mIsBIOSCapInitDone = true;
+    return rc;
+}
 
-    //This is a must step as CurrentPayloadType == INVALID_IMG_TYPE after module init or each transfer complete.
-    if (currentPayloadType >= OOBImageType::invalidType) {
-        phosphor::logging::log<phosphor::logging::level::ERR>("invalid image type");
+ipmi_ret_t ipmiOOBGetBIOSCap(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
+                             ipmi_request_t request, ipmi_response_t response,
+                             ipmi_data_len_t dataLen, ipmi_context_t context)
+{
+    ipmi_ret_t rc = IPMI_CC_OK;
+    getBIOSCapabilitiesResp *getBIOSCapabilties =
+        reinterpret_cast<getBIOSCapabilitiesResp *>(request);
+
+    if (mIsBIOSCapInitDone)
+    {
+        getBIOSCapabilties->reserved1 = 0;
+        getBIOSCapabilties->reserved2 = 0;
+        getBIOSCapabilties->reserved3 = 0;
+        getBIOSCapabilties->OOBCapability = mBIOSCapabilities.OOBCapability;
+        *dataLen = sizeof(getBIOSCapabilitiesResp);
+        return rc;
+    }
+    else
+    {
+        *dataLen = 0;
+        return OOBCompleteCode::compcodePayloadLengthIllegal;
+    }
+}
+
+ipmi_ret_t ipmiOOBSetPayload(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
+                             ipmi_request_t request, ipmi_response_t response,
+                             ipmi_data_len_t dataLen, ipmi_context_t context)
+{
+    SetPayloadReq *pPayloadReq = reinterpret_cast<SetPayloadReq *>(request);
+    SetPayloadResp *pPayloadResp = reinterpret_cast<SetPayloadResp *>(response);
+    uint32_t u32Crc;
+    size_t returnSize = 0;
+    OOBImageType currentPayloadType = pPayloadReq->payloadType;
+    ipmi_ret_t rc = IPMI_CC_OK;
+
+    // This is a must step as CurrentPayloadType == INVALID_IMG_TYPE after
+    // module init or each transfer complete.
+    if (currentPayloadType >= OOBImageType::invalidType)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "invalid image type");
         *dataLen = 0;
         return OOBCompleteCode::compcodePayloadTypeNotSupported;
     }
+    phosphor::logging::log<phosphor::logging::level::DEBUG>(
+        "PARASEL",
+        phosphor::logging::entry("PARASEL=%x", pPayloadReq->paraSel));
 
-    phosphor::logging::log<phosphor::logging::level::DEBUG>("PARASEL", phosphor::logging::entry("PARASEL=%x", pPayloadReq->paraSel));
-
-    switch(pPayloadReq->paraSel) {
+    switch (pPayloadReq->paraSel)
+    {
         case TransferState::PasswdAuth:
             break;
         case TransferState::StartTransfer:
@@ -68,11 +121,170 @@ ipmi_ret_t ipmiOOBSetPayload(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
         case TransferState::UserAbort:
             break;
         default:
-            *dataLen = 0;            
+            *dataLen = 0;
             rc = IPMI_CC_INVALID_FIELD_REQUEST;
     }
     gPayload[1].status = pPayloadReq->paraSel;
 
+    return rc;
+}
+
+ipmi_ret_t ipmiOOBGetPayload(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
+                             ipmi_request_t request, ipmi_response_t response,
+                             ipmi_data_len_t dataLen, ipmi_context_t context)
+{
+    GetPayloadReq *pPayloadReq = reinterpret_cast<GetPayloadReq *>(request);
+    GetPayloadResp *pPayloadResp = reinterpret_cast<GetPayloadResp *>(response);
+    uint32_t u32Crc;
+    uint8_t *Imgbuffer;
+    size_t returnSize = 0;
+    OOBImageType currentPayloadType =
+        static_cast<OOBImageType>(pPayloadReq->Input.Para0.payloadType);
+    ipmi_ret_t rc = IPMI_CC_OK;
+
+    // This is a must step as CurrentPayloadType == INVALID_IMG_TYPE after
+    // module init or each transfer complete.
+    if (currentPayloadType >= OOBImageType::invalidType)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "invalid image type");
+        *dataLen = 0;
+        return OOBCompleteCode::compcodePayloadTypeNotSupported;
+    }
+
+    phosphor::logging::log<phosphor::logging::level::DEBUG>(
+        "ipmiOOBGetPayload",
+        phosphor::logging::entry("ipmiOOBGetPayload Param=%x",
+                                 pPayloadReq->paraSel));
+
+    switch (pPayloadReq->paraSel)
+    {
+        case GetPayloadParameter::PayloadInfo:
+            returnSize = 0;
+            pPayloadResp->Output.Para0.payloadType =
+                pPayloadReq->Input.Para0.payloadType;
+            pPayloadResp->Output.Para0.payloadVersion =
+                gPayload[pPayloadReq->Input.Para0.payloadType].payloadVersion;
+            pPayloadResp->Output.Para0.totalPayloadlength =
+                gPayload[pPayloadReq->Input.Para0.payloadType].givenPayloadSize;
+            pPayloadResp->Output.Para0.totalPayloadchecksum =
+                gPayload[pPayloadReq->Input.Para0.payloadType]
+                    .givenPayloadChecksum;
+            pPayloadResp->Output.Para0.payloadCurrentStatus =
+                gPayload[pPayloadReq->Input.Para0.payloadType].status;
+            pPayloadResp->Output.Para0.timeStamp =
+                gPayload[pPayloadReq->Input.Para0.payloadType].uploadTimeStamp;
+            pPayloadResp->Output.Para0.payloadFlag =
+                gPayload[pPayloadReq->Input.Para0.payloadType].payloadFlag;
+            rc = IPMI_CC_OK;
+            rc = sizeof(pPayloadResp->Output.Para0);
+
+            break;
+        case GetPayloadParameter::PayloadData:
+            if (pPayloadReq->Input.Para1.payloadLength == 0)
+            {
+                rc = IPMI_CC_INVALID_FIELD_REQUEST;
+                *dataLen = 0;
+                break;
+            }
+            Imgbuffer = new uint8_t(pPayloadReq->Input.Para1.payloadLength);
+            if (Imgbuffer != NULL)
+            {
+                pPayloadResp->Output.Para1.actualPayloadlength =
+                    imgReadFromFile(
+                        pPayloadReq->Input.Para1.payloadType, Imgbuffer,
+                        pPayloadReq->Input.Para1.payloadOffset,
+                        pPayloadReq->Input.Para1.payloadLength,
+                        &pPayloadResp->Output.Para1.actualPayloadChecksum);
+                rc = IPMI_CC_OK;
+                std::copy(pPayloadResp->Output.Para1.payloadData,
+                          pPayloadResp->Output.Para1.payloadData +
+                              pPayloadResp->Output.Para1.actualPayloadlength,
+                          Imgbuffer);
+                *dataLen =
+                    pPayloadResp->Output.Para1.actualPayloadlength +
+                    sizeof(pPayloadResp->Output.Para1.actualPayloadlength) +
+                    sizeof(pPayloadResp->Output.Para1.actualPayloadChecksum) +
+                    sizeof(pPayloadResp->Output.Para1.payloadType);
+                delete Imgbuffer;
+            }
+            else
+            {
+                rc = OOBCompleteCode::compcodePayloadTypeNotSupported;
+                *dataLen = 0;
+            }
+
+            break;
+        case GetPayloadParameter::PayloadStatus:
+            pPayloadResp->Output.Para2.status =
+                gPayload[pPayloadReq->Input.Para0.payloadType].status;
+            rc = IPMI_CC_OK;
+            *dataLen = sizeof(pPayloadResp->Output.Para2.status);
+            break;
+
+        default:
+            *dataLen = 0;
+            rc = IPMI_CC_INVALID_FIELD_REQUEST;
+    }
+
+    return rc;
+}
+
+ipmi_ret_t ipmiOOBSetBIOSPwdHashInfo(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
+                                     ipmi_request_t request,
+                                     ipmi_response_t response,
+                                     ipmi_data_len_t dataLen,
+                                     ipmi_context_t context)
+{
+    setBIOSHashInfoReq *pBIOSHashinfo =
+        reinterpret_cast<setBIOSHashInfoReq *>(request);
+    ipmi_ret_t rc = IPMI_CC_OK;
+    *dataLen = 0;
+    if (pBIOSHashinfo->hashAlgo != 0)
+    { // Atpresent, we are supporting only SHA 256 - HASH algo in BIOS side
+        rc = IPMI_CC_INVALID_FIELD_REQUEST;
+        return rc;
+    }
+    std::copy(mBIOSHashInfo.BIOSPwdHash,
+              mBIOSHashInfo.BIOSPwdHash + maxHashSize,
+              pBIOSHashinfo->BIOSPwdHash);
+    std::copy(mBIOSHashInfo.BIOSPwdHash,
+              mBIOSHashInfo.BIOSPwdHash + maxSeedSize,
+              pBIOSHashinfo->BIOSPwdSeed);
+    mBIOSHashInfo.hashAlgo = pBIOSHashinfo->hashAlgo;
+    return rc;
+}
+
+ipmi_ret_t ipmiOOBGetBIOSPwdHash(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
+                                 ipmi_request_t request,
+                                 ipmi_response_t response,
+                                 ipmi_data_len_t dataLen,
+                                 ipmi_context_t context)
+{
+    getUserBIOSHashResp *getUserBIOSHash =
+        reinterpret_cast<getUserBIOSHashResp *>(response);
+    ipmi_ret_t rc = IPMI_CC_OK;
+    if (IsUserPwdInitDone)
+    {
+        std::copy(getUserBIOSHash->userPwdHash,
+                  getUserBIOSHash->userPwdHash + maxHashSize, mUserPwdHash);
+        *dataLen = sizeof(getUserBIOSHashResp);
+    }
+    else
+    {
+        *dataLen = 0;
+        rc = OOBCompleteCode::compcodePayloadTypeNotSupported;
+    }
+    return rc;
+}
+
+ipmi_ret_t ipmiOOBUpdateOOBStatus(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
+                                  ipmi_request_t request,
+                                  ipmi_response_t response,
+                                  ipmi_data_len_t dataLen,
+                                  ipmi_context_t context)
+{
+    ipmi_ret_t rc = IPMI_CC_OK;
     return rc;
 }
 
@@ -85,12 +297,34 @@ static void initOOB(void)
 
     ipmiPrintAndRegister(
         netfnIntcOEMGeneral,
-        static_cast<ipmi_cmd_t>(
-            IPMINetfnIntelOEMGeneralCmd::cmdSetPayload),
+        static_cast<ipmi_cmd_t>(IPMINetfnIntelOEMGeneralCmd::cmdSetBIOSCap),
+        NULL, ipmiOOBSetBIOSCap, PRIVILEGE_ADMIN);
+    ipmiPrintAndRegister(
+        netfnIntcOEMGeneral,
+        static_cast<ipmi_cmd_t>(IPMINetfnIntelOEMGeneralCmd::cmdGetBIOSCap),
+        NULL, ipmiOOBGetBIOSCap, PRIVILEGE_USER);
+    ipmiPrintAndRegister(
+        netfnIntcOEMGeneral,
+        static_cast<ipmi_cmd_t>(IPMINetfnIntelOEMGeneralCmd::cmdSetPayload),
         NULL, ipmiOOBSetPayload, PRIVILEGE_ADMIN);
-
+    ipmiPrintAndRegister(
+        netfnIntcOEMGeneral,
+        static_cast<ipmi_cmd_t>(IPMINetfnIntelOEMGeneralCmd::cmdGetPayload),
+        NULL, ipmiOOBGetPayload, PRIVILEGE_USER);
+    ipmiPrintAndRegister(
+        netfnIntcOEMGeneral,
+        static_cast<ipmi_cmd_t>(
+            IPMINetfnIntelOEMGeneralCmd::cmdSetBIOSPwdHashInfo),
+        NULL, ipmiOOBSetBIOSPwdHashInfo, PRIVILEGE_ADMIN);
+    ipmiPrintAndRegister(
+        netfnIntcOEMGeneral,
+        static_cast<ipmi_cmd_t>(IPMINetfnIntelOEMGeneralCmd::cmdGetBIOSPwdHash),
+        NULL, ipmiOOBGetBIOSPwdHash, PRIVILEGE_ADMIN);
+    ipmiPrintAndRegister(netfnIntcOEMGeneral,
+                         static_cast<ipmi_cmd_t>(
+                             IPMINetfnIntelOEMGeneralCmd::cmdUpdateOOBStatus),
+                         NULL, ipmiOOBUpdateOOBStatus, PRIVILEGE_ADMIN);
     return;
 }
 
-}
-
+} // namespace ipmi
