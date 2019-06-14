98268f0cd3cb2739a010131022a5de27e583886b
Deepak Kumar Sahu <deepakx.sahu@intel.com>
2019-06-14 17:35:21 +0000

    bridgingcommands: Get&ClearMessageFlags to new API
    Rewrite "ipmiAppGetMessageFlags" and "ipmiAppClearMessageFlags"
    command to use the newly introduced IPMI provider API.
    
    Tested:
    Verified using ipmitool GetMessageFlags and ClearMessageFlags,
    behavior is same before and after the changes.
    
    Test Result:
    
    Get Message Flags
    ipmitool raw 0x06 0x31
    Output: 02
    Clear Message Flags
    ipmitool raw 0x06 0x30 0x00
    Output:
    
    Signed-off-by: Deepak Kumar Sahu <deepakx.sahu@intel.com>
    Change-Id: I90159a0dcb377a3a593c43998d78a14d0d1e1b6a

--- include/bridgingcommands.hpp
+++ include/bridgingcommands.hpp
@@ -314,6 +314,8 @@ class Bridging
   public:
     Bridging() = default;
 
+    std::vector<IpmbResponse> getResponseQueue();
+
     ipmi_return_codes sendMessageHandler(ipmi_request_t request,
                                          ipmi_response_t response,
                                          ipmi_data_len_t dataLen);
@@ -321,15 +323,6 @@ class Bridging
     ipmi_return_codes getMessageHandler(ipmi_request_t request,
                                         ipmi_response_t response,
                                         ipmi_data_len_t dataLen);
-
-    ipmi_return_codes clearMessageFlagsHandler(ipmi_request_t request,
-                                               ipmi_response_t response,
-                                               ipmi_data_len_t dataLen);
-
-    ipmi_return_codes getMessageFlagsHandler(ipmi_request_t request,
-                                             ipmi_response_t response,
-                                             ipmi_data_len_t dataLen);
-
     enum IpmiAppBridgingCmds
     {
         ipmiCmdClearMessageFlags = 0x30,
