18060bddad0b9a8b6d08a823e9a4da27aa5a4b01
Yong Li <yong.b.li@linux.intel.com>
2019-08-07 18:45:01 +0800

    intel-ipmi-oem: add slot I2C master RW command
    This command is used to perform i2c master read/write on PCIE slots.
    
    Tested:
    Read the i2c device 0x56 on slot 2 of riser 2,
    the below two commands have the same result:
    
    ipmitool raw 0x3e 0x52 0 0xa 0x56 8 0
    i2cget -f -y 14 0x56 0  i 8
    
    Signed-off-by: Yong Li <yong.b.li@linux.intel.com>
    Change-Id: I03872296345d180571db2205a9a30c08f28b69b6

--- src/manufacturingcommands.cpp
+++ src/manufacturingcommands.cpp
@@ -13,15 +13,51 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 */
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
 
+#include <filesystem>
+#include <fstream>
 #include <ipmid/api.hpp>
 #include <manufacturingcommands.hpp>
+#include <nlohmann/json.hpp>
 #include <oemcommands.hpp>
 
 namespace ipmi
 {
 
 Manufacturing mtm;
+
+typedef struct
+{
+    uint8_t busId;
+    uint8_t slaveAddr;
+    uint8_t slaveAddrMask;
+    std::vector<uint8_t> data;
+    std::vector<uint8_t> dataMask;
+} i2cMasterWRWhitelist;
+
+static std::vector<i2cMasterWRWhitelist>& getWRWhitelist()
+{
+    static std::vector<i2cMasterWRWhitelist> wrWhitelist;
+    return wrWhitelist;
+}
+
+static constexpr const char* i2cMasterWRWhitelistFile =
+    "/usr/share/ipmi-providers/master_write_read_white_list.json";
+
+static constexpr uint8_t slotAddressTypeBus = 0;
+static constexpr uint8_t slotAddressTypeUniqueid = 1;
+static constexpr uint8_t slotI2CMaxRead = 35;
+static constexpr uint8_t slotBBNumMask = 0x7;
+static constexpr uint8_t slotRiserSlotNumMask = 0x38;
+static constexpr int base_16 = 16;
+static constexpr const char* filtersStr = "filters";
+static constexpr const char* busIdStr = "busId";
+static constexpr const char* slaveAddrStr = "slaveAddr";
+static constexpr const char* slaveAddrMaskStr = "slaveAddrMask";
+static constexpr const char* cmdStr = "command";
+static constexpr const char* cmdMaskStr = "commandMask";
 
 static auto revertTimeOut =
     std::chrono::duration_cast<std::chrono::microseconds>(
@@ -230,6 +266,310 @@ int8_t Manufacturing::disablePidControlService(const bool disable)
     return 0;
 }
 
+inline std::vector<uint8_t> convertStringToData(const std::string& command)
+{
+    std::istringstream iss(command);
+    std::string token;
+    std::vector<uint8_t> dataValue;
+    while (std::getline(iss, token, ' '))
+    {
+        dataValue.emplace_back(
+            static_cast<uint8_t>(std::stoul(token, nullptr, base_16)));
+    }
+    return dataValue;
+}
+
+static bool populateI2CMasterWRWhitelist()
+{
+    nlohmann::json data = nullptr;
+    std::ifstream jsonFile(i2cMasterWRWhitelistFile);
+
+    if (!jsonFile.good())
+    {
+        phosphor::logging::log<phosphor::logging::level::WARNING>(
+            "i2c white list file not found!",
+            phosphor::logging::entry("FILE_NAME=%s", i2cMasterWRWhitelistFile));
+        return false;
+    }
+
+    try
+    {
+        data = nlohmann::json::parse(jsonFile, nullptr, false);
+    }
+    catch (nlohmann::json::parse_error& e)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Corrupted i2c white list config file",
+            phosphor::logging::entry("FILE_NAME=%s", i2cMasterWRWhitelistFile),
+            phosphor::logging::entry("MSG=%s", e.what()));
+        return false;
+    }
+
+    try
+    {
+        // Example JSON Structure format
+        // "filters": [
+        //    {
+        //      "Description": "Allow full read - ignore first byte write value
+        //      for 0x40 to 0x4F",
+        //      "busId": "0x01",
+        //      "slaveAddr": "0x40",
+        //      "slaveAddrMask": "0x0F",
+        //      "command": "0x00",
+        //      "commandMask": "0xFF"
+        //    },
+        //    {
+        //      "Description": "Allow full read - first byte match 0x05 and
+        //      ignore second byte",
+        //      "busId": "0x01",
+        //      "slaveAddr": "0x57",
+        //      "slaveAddrMask": "0x00",
+        //      "command": "0x05 0x00",
+        //      "commandMask": "0x00 0xFF"
+        //    },]
+
+        nlohmann::json filters = data[filtersStr].get<nlohmann::json>();
+        std::vector<i2cMasterWRWhitelist>& whitelist = getWRWhitelist();
+        for (const auto& it : filters.items())
+        {
+            nlohmann::json filter = it.value();
+            if (filter.is_null())
+            {
+                phosphor::logging::log<phosphor::logging::level::ERR>(
+                    "Corrupted I2C master write read whitelist config file",
+                    phosphor::logging::entry("FILE_NAME=%s",
+                                             i2cMasterWRWhitelistFile));
+                return false;
+            }
+            const std::vector<uint8_t>& writeData =
+                convertStringToData(filter[cmdStr].get<std::string>());
+            const std::vector<uint8_t>& writeDataMask =
+                convertStringToData(filter[cmdMaskStr].get<std::string>());
+            if (writeDataMask.size() != writeData.size())
+            {
+                phosphor::logging::log<phosphor::logging::level::ERR>(
+                    "I2C master write read whitelist filter "
+                    "mismatch for command & mask size");
+                return false;
+            }
+            whitelist.push_back(
+                {static_cast<uint8_t>(std::stoul(
+                     filter[busIdStr].get<std::string>(), nullptr, base_16)),
+                 static_cast<uint8_t>(
+                     std::stoul(filter[slaveAddrStr].get<std::string>(),
+                                nullptr, base_16)),
+                 static_cast<uint8_t>(
+                     std::stoul(filter[slaveAddrMaskStr].get<std::string>(),
+                                nullptr, base_16)),
+                 writeData, writeDataMask});
+        }
+        if (whitelist.size() != filters.size())
+        {
+            phosphor::logging::log<phosphor::logging::level::ERR>(
+                "I2C master write read whitelist filter size mismatch");
+            return false;
+        }
+    }
+    catch (std::exception& e)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "I2C master write read whitelist unexpected exception",
+            phosphor::logging::entry("ERROR=%s", e.what()));
+        return false;
+    }
+    return true;
+}
+
+static inline bool isWriteDataWhitelisted(const std::vector<uint8_t>& data,
+                                          const std::vector<uint8_t>& dataMask,
+                                          const std::vector<uint8_t>& writeData)
+{
+    std::vector<uint8_t> processedDataBuf(data.size());
+    std::vector<uint8_t> processedReqBuf(dataMask.size());
+    std::transform(writeData.begin(), writeData.end(), dataMask.begin(),
+                   processedReqBuf.begin(), std::bit_or<uint8_t>());
+    std::transform(data.begin(), data.end(), dataMask.begin(),
+                   processedDataBuf.begin(), std::bit_or<uint8_t>());
+
+    return (processedDataBuf == processedReqBuf);
+}
+
+static bool isCmdWhitelisted(uint8_t busId, uint8_t slaveAddr,
+                             std::vector<uint8_t>& writeData)
+{
+    std::vector<i2cMasterWRWhitelist>& whiteList = getWRWhitelist();
+
+    for (const auto& wlEntry : whiteList)
+    {
+        if ((busId == wlEntry.busId) &&
+            ((slaveAddr | wlEntry.slaveAddrMask) ==
+             (wlEntry.slaveAddr | wlEntry.slaveAddrMask)))
+        {
+            const std::vector<uint8_t>& dataMask = wlEntry.dataMask;
+            // Skip as no-match, if requested write data is more than the
+            // write data mask size
+            if (writeData.size() > dataMask.size())
+            {
+                continue;
+            }
+            if (isWriteDataWhitelisted(wlEntry.data, dataMask, writeData))
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+static int getBusNum(const uint8_t& riserNum, const uint8_t& slotNum,
+                     uint8_t& busNum)
+{
+
+    std::string path = "/dev/i2c-mux/Riser_" + std::to_string(riserNum) +
+                       "_Mux/Pcie_Slot_" + std::to_string(slotNum);
+
+    if (std::filesystem::exists(path) && std::filesystem::is_symlink(path))
+    {
+        std::string link = std::filesystem::read_symlink(path).filename();
+        size_t findDash = link.find("-");
+        if (findDash == std::string::npos || link.size() <= findDash + 1)
+        {
+            phosphor::logging::log<phosphor::logging::level::ERR>(
+                "Master write read command: Error finding device from symlink");
+            return -1;
+        }
+
+        size_t bus = 0;
+        bus = std::stoi(link.substr(findDash + 1));
+        busNum = bus;
+    }
+    else
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+/** @brief implements slot master write read IPMI command which can be used for
+ * low-level I2C/SMBus write, read or write-read access for PCIE slots
+ *  @param busNum - bus number
+ *  @param reserved - skip 2 bit
+ *  @param addressType - address type
+ *  @param slotNum - slot number
+ *  @param slaveAddr - slave address
+ *  @param readCount - number of bytes to be read
+ *  @param writeData - data to be written
+ *
+ *  @returns IPMI completion code plus response data
+ */
+ipmi::RspType<std::vector<uint8_t>>
+    appSlotI2C(uint4_t busNum, uint2_t reserved, uint2_t addressType,
+               uint8_t slotNum, uint8_t slaveAddr, uint8_t readCount,
+               std::vector<uint8_t> writeData)
+{
+    i2c_rdwr_ioctl_data msgReadWrite = {0};
+    i2c_msg i2cmsg[2] = {0};
+    uint8_t busId = 0;
+    int ret = 0;
+
+    if (addressType == slotAddressTypeBus)
+    {
+        ret = getBusNum((slotNum & slotBBNumMask),
+                        ((slotNum & slotRiserSlotNumMask) >> 3), busId);
+        if (ret)
+        {
+            phosphor::logging::log<phosphor::logging::level::ERR>(
+                "Master write read command: Cannot get BusID");
+            return ipmi::responseInvalidFieldRequest();
+        }
+    }
+    else if (addressType == slotAddressTypeUniqueid)
+    {
+        if (mtm.getAccessLvl() < MtmLvl::mtmAvailable)
+        {
+            return ipmi::responseInsufficientPrivilege();
+        }
+        busId = slotNum;
+    }
+
+    if (readCount > slotI2CMaxRead)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Master write read command: Read count exceeds limit");
+        return ipmi::responseParmOutOfRange();
+    }
+
+    const size_t writeCount = writeData.size();
+    if (!readCount && !writeCount)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Master write read command: Read & write count are 0");
+        return ipmi::responseInvalidFieldRequest();
+    }
+
+    if (!isCmdWhitelisted(static_cast<uint8_t>(busId),
+                          static_cast<uint8_t>(slaveAddr), writeData))
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Master write read request blocked!",
+            phosphor::logging::entry("BUS=%d", static_cast<uint8_t>(busId)),
+            phosphor::logging::entry("ADDR=0x%x",
+                                     static_cast<uint8_t>(slaveAddr)));
+        return ipmi::responseInvalidFieldRequest();
+    }
+
+    std::vector<uint8_t> readBuf(readCount);
+    std::string i2cBus =
+        "/dev/i2c-" + std::to_string(static_cast<uint8_t>(busId));
+
+    int i2cDev = ::open(i2cBus.c_str(), O_RDWR | O_CLOEXEC);
+    if (i2cDev < 0)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Failed to open i2c bus",
+            phosphor::logging::entry("BUS=%s", i2cBus.c_str()));
+        return ipmi::responseInvalidFieldRequest();
+    }
+
+    int msgCount = 0;
+    if (writeCount)
+    {
+        i2cmsg[msgCount].addr = static_cast<uint8_t>(slaveAddr);
+        i2cmsg[msgCount].flags = 0x00;
+        i2cmsg[msgCount].len = writeCount;
+        i2cmsg[msgCount].buf = writeData.data();
+        msgCount++;
+    }
+    if (readCount)
+    {
+        i2cmsg[msgCount].addr = static_cast<uint8_t>(slaveAddr);
+        i2cmsg[msgCount].flags = I2C_M_RD;
+        i2cmsg[msgCount].len = readCount;
+        i2cmsg[msgCount].buf = readBuf.data();
+        msgCount++;
+    }
+
+    msgReadWrite.msgs = i2cmsg;
+    msgReadWrite.nmsgs = msgCount;
+
+    ret = ::ioctl(i2cDev, I2C_RDWR, &msgReadWrite);
+    ::close(i2cDev);
+
+    if (ret < 0)
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Master write read: Failed",
+            phosphor::logging::entry("RET=%d", ret));
+        return ipmi::responseUnspecifiedError();
+    }
+    if (readCount)
+    {
+        readBuf.resize(msgReadWrite.msgs[msgCount - 1].len);
+    }
+    return ipmi::responseSuccess(readBuf);
+}
+
 ipmi::RspType<uint8_t,                // Signal value
               std::optional<uint16_t> // Fan tach value
               >
@@ -238,7 +578,7 @@ ipmi::RspType<uint8_t,                // Signal value
 {
     if (mtm.getAccessLvl() < MtmLvl::mtmAvailable)
     {
-        return ipmi::responseInvalidCommand();
+        return ipmi::responseInsufficientPrivilege();
     }
 
     SmSignalGet signalType = static_cast<SmSignalGet>(signalTypeByte);
@@ -556,10 +896,19 @@ void register_mtm_commands()
         static_cast<ipmi::Cmd>(IPMINetFnIntelOemGeneralCmds::GetSmSignal),
         ipmi::Privilege::User, ipmi::appMTMGetSignal);
 
+    // Note: For security reason, this command will be registered only when
+    // there are proper I2C Master write read whitelist
+    if (ipmi::populateI2CMasterWRWhitelist())
+    {
+        ipmi::registerHandler(
+            ipmi::prioOemBase, ipmi::netFnOemEight,
+            static_cast<ipmi_cmd_t>(IPMINetfnIntelOEMAppCmd::slotI2C),
+            ipmi::Privilege::User, ipmi::appSlotI2C);
+    }
+
     ipmi_register_callback(
         netfnIntcOEMGeneral,
         static_cast<ipmi_cmd_t>(IPMINetFnIntelOemGeneralCmds::SetSmSignal),
         NULL, ipmi::ipmi_app_mtm_set_signal, PRIVILEGE_USER);
-
     return;
 }
