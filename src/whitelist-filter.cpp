4e96cfb0b2b8704dd7fe628349b093f088417331
arun-pm <arun.p.m@linux.intel.com>
2020-02-12 18:10:32 +0530

    Adding KCS-SMM channel restriction
    Added support to filter the SMM commands as per the updated whitelist conf.
    
    Tested:
    When RestrictionMode is Provisioning
      Executed a blocked command on SMM and SystemInterface(Success).
    When RestrictionMode is ProvisionedHostWhitelist
      Verified following condition over SMM channel
      1) Executed a command whitlisted on SMM channel(Success).
      2) Executed a command blocked on SMM channel(Denied with CC D5).
      Verified following condition over SystemInterface
      3) Executed a command whitlisted on SystemInterface(Success).
      4) Executed a command blocked on SystemInterface(Denied with CC D5).
    When RestrictionMode is ProvisionedHostDisabled
      Executed an allowed command on both SMM and SystemInterface
    (Denied with CC D5).
    
    Signed-off-by: arun-pm <arun.p.m@linux.intel.com>
    Change-Id: If99fd90ea9faf1d3440d10ecfc385ab3c907c736

--- src/whitelist-filter.cpp
+++ src/whitelist-filter.cpp
@@ -54,6 +54,7 @@ class WhitelistFilter
 
     RestrictionMode::Modes restrictionMode = restrictionModeRestricted;
     bool postCompleted = false;
+    uint8_t channelSMM = -1;
     std::shared_ptr<sdbusplus::asio::connection> bus;
     std::unique_ptr<sdbusplus::bus::match::match> modeChangeMatch;
     std::unique_ptr<sdbusplus::bus::match::match> modeIntfAddedMatch;
@@ -66,6 +67,29 @@ class WhitelistFilter
         "xyz.openbmc_project.State.OperatingSystem.Status";
 };
 
+static inline uint8_t getSMMChannel()
+{
+    ipmi::ChannelInfo chInfo;
+
+    for (uint8_t channel = 0; channel < ipmi::maxIpmiChannels; channel++)
+    {
+        if (ipmi::getChannelInfo(channel, chInfo) != ipmi::ccSuccess)
+        {
+            log<level::ERR>("Failed to get Channel Info",
+                            entry("CHANNEL=%d", channel));
+            continue;
+        }
+
+        if (static_cast<ipmi::EChannelMediumType>(chInfo.mediumType) ==
+                ipmi::EChannelMediumType::systemInterface &&
+            channel != ipmi::channelSystemIface)
+        {
+            return channel;
+        }
+    }
+    return -1;
+}
+
 WhitelistFilter::WhitelistFilter()
 {
     bus = getSdBus();
@@ -76,6 +100,16 @@ WhitelistFilter::WhitelistFilter()
                          [this](ipmi::message::Request::ptr request) {
                              return filterMessage(request);
                          });
+
+    channelSMM = getSMMChannel();
+    if (channelSMM == -1)
+    {
+        log<level::ERR>("Unable to find SMM Channel Info");
+    }
+    else
+    {
+        log<level::INFO>("SMM channel number", entry("CHANNEL=%d", channelSMM));
+    }
 
     // wait until io->run is going to fetch RestrictionMode
     post_work([this]() { postInit(); });
@@ -307,7 +341,8 @@ ipmi::Cc WhitelistFilter::filterMessage(ipmi::message::Request::ptr request)
         });
 
     // no special handling for non-system-interface channels
-    if (request->ctx->channel != ipmi::channelSystemIface)
+    if (!(request->ctx->channel == ipmi::channelSystemIface ||
+          request->ctx->channel == channelSMM))
     {
         if (!whitelisted)
         {
