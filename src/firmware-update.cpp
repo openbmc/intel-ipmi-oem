f1585266cb599f15d9b711efb45dc2f358eadadf
srikanta mondal <srikantax.mondal@intel.com>
2020-07-27 23:49:14 +0000

    GetFwVersionInfo: Fix for Firmware aux version
    Issue: Get Version Information return incorrect Firmware AUX version
    
    FIX: Firmware aux version was parsing incorrectly. Do the parsing by
         correct regex pattern of BMC version.
    
    Tested:
    Verified using ipmitool raw commands
    Before fix:
    Command: ipmitool raw 0x08 0x20 // Get version Information
    Response: 01 02 00 67 00 00 00 00 00 00 00 00 00 00 00 00
    
    After fix:
    Command: ipmitool raw 0x08 0x20 // Get version Information
    Response: 02 01 00 48 0e f4 7b 53 00 00 00 00 00 00 00 00
              02 00 70 01 00 00 00 00 00 00 00 00 00 00 00
    
    Signed-off-by: srikanta mondal <srikantax.mondal@intel.com>
    Change-Id: I7c2baf8a0da15e3ca4db5d6f9d6de7bf739aa755

--- src/firmware-update.cpp
+++ src/firmware-update.cpp
@@ -20,6 +20,8 @@
 #include <sdbusplus/server/object.hpp>
 #include <sdbusplus/timer.hpp>
 #include <types.hpp>
+#include <appcommands.hpp>
+#include <byteswap.h>
 
 #include <chrono>
 #include <cstdint>
@@ -853,9 +855,22 @@ ipmi::RspType<uint8_t, std::vector<fwVersionInfoType>> ipmiGetFwVersionInfo()
         uint32_t buildNum = 0;
         try
         {
-            majorNum = std::stoul(splitVer[0], nullptr, 16);
-            minorNum = std::stoul(splitVer[1], nullptr, 16);
-            buildNum = std::stoul(splitVer[2], nullptr, 16);
+            std::optional<ipmi::MetaRevision> rev = ipmi::convertIntelVersion(verStr);
+            if (rev.has_value())
+            {
+                ipmi::MetaRevision revision = rev.value();
+                majorNum = revision.major;
+                minorNum = revision.minor;
+                uint32_t hash = std::stoul(revision.metaHash, 0, 16);
+                hash = bswap_32(hash);
+                buildNum = (revision.buildNo & 0xFF) + (hash & 0xFFFFFF00);
+            }
+            else
+            {
+                majorNum = std::stoul(splitVer[0], nullptr, 16);
+                minorNum = std::stoul(splitVer[1], nullptr, 16);
+                buildNum = std::stoul(splitVer[2], nullptr, 16);
+            }
         }
         catch (const std::exception& e)
         {
