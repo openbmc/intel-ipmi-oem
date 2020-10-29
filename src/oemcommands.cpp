f42f58117a07260a0fd4f163fcc7ae747c513f98
Jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
2020-10-29 14:42:59 +0000

    ipmiUserSetUserName argument change
    ipmiUserSetUserName API signature is changed to use string variable.
    
    Tested:
    Verified using cmdtool utility command
    Command: cmdtool.efi 20 c0 5a 64 65 62 75 67 75 73 65 72 0 0 0 0 0 0 0
     30 70 65 6e 42 6d 63 31 0 0 0 0 0 0 0 0 0 0 0 0
    Response: User is created successfully
    Command: ipmitool user list 1 //User list for channel 1
    Response:
    ID  Name             Callin  Link Auth  IPMI Msg   Channel Priv Limit
    1   root             false   true       true       ADMINISTRATOR
    2   debuguser        true    false      false      NO ACCESS
    3   user3            true    false      false      NO ACCESS
    4   user4            true    false      false      NO ACCESS
    
    Signed-off-by: Jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
    Change-Id: I3462b7ea4928f3d6bb370c4c97fbe9cb543a3375

--- src/oemcommands.cpp
+++ src/oemcommands.cpp
@@ -1096,7 +1096,10 @@ ipmi::RspType<> ipmiOEMSetUser2Activation(
 
     // ipmiUserSetUserName correctly handles char*, possibly non-null
     // terminated strings using ipmiMaxUserName size
-    auto userNameRaw = reinterpret_cast<const char*>(userName.data());
+    size_t nameLen = strnlen(reinterpret_cast<const char*>(userName.data()),
+                             sizeof(userName));
+    const std::string userNameRaw(
+        reinterpret_cast<const char*>(userName.data()), nameLen);
 
     if (ipmi::ccSuccess == ipmiUserSetUserName(ipmiDefaultUserId, userNameRaw))
     {
