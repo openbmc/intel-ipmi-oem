f3eac29e90415ff3e5aa5e7618ba5ec366c2b7ea
Jason M. Bills <jason.m.bills@linux.intel.com>
2021-06-15 12:46:13 -0700

    Fix build issues
    Fix casting issues that are causing the build to fail.
    
    Change-Id: Id6eb6b1fef335f88e7e12074f712b82ecd1a233a
    Signed-off-by: Jason M. Bills <jason.m.bills@linux.intel.com>

--- src/chassiscommands.cpp
+++ src/chassiscommands.cpp
@@ -512,7 +512,7 @@ ipmi::RspType<bool,    // Power is on
     constexpr bool coolingFanFault = false;
     // chassisIdentifySupport set because this command is implemented
     constexpr bool chassisIdentifySupport = true;
-    uint2_t chassisIdentifyState = chassisIDState;
+    uint2_t chassisIdentifyState = static_cast<uint8_t>(chassisIDState);
     constexpr bool sleepButtonDisabled = false;
     constexpr bool sleepButtonDisableAllow = false;
 
