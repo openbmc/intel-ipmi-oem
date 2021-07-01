3555c4205d562497d1bc04353ba04cf422773f82
Jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
2021-07-01 17:09:39 +0000

    oemcommands: Validate reserved field content
    In "Set Processor Error Configuration" command,  byte-1’s [7-2],
    byte-2, byte-3's [7-1],  bits are reserved and in "Disable BMC System
    Reset Action" command,  byte-1’s [7-1] bits are reserved. So return
    "InvalidFieldRequest" when reserved content is non-zero.
    
    Tested:
    Command : ipmitool raw 0x30 0x9b 0x03 0x0 0x04
                                       //Set Processor Error Configuration
    Response: Unable to send RAW command (channel=0x0 netfn=0x30 lun=0x0
              cmd=0x9b rsp=0xcc): Invalid data field in request
    Command : ipmitool raw 0x30 0x42 0x03 //Disable BMC System Reset Action
    Response: Unable to send RAW command (channel=0x0 netfn=0x30 lun=0x0
              cmd=0x42 rsp=0xcc): Invalid data field in request
    
    Signed-off-by: Jayaprakash Mutyala <mutyalax.jayaprakash@intel.com>
    Change-Id: I84a6e3be21d6101824867ff9f92b83be2e6aa406

--- src/oemcommands.cpp
+++ src/oemcommands.cpp
@@ -229,6 +229,11 @@ ipmi_ret_t ipmiOEMSetSystemGUID(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
 ipmi::RspType<> ipmiOEMDisableBMCSystemReset(bool disableResetOnSMI,
                                              uint7_t reserved1)
 {
+    if (reserved1)
+    {
+        return ipmi::responseInvalidFieldRequest();
+    }
+
     std::shared_ptr<sdbusplus::asio::connection> busp = getSdBus();
 
     try
@@ -789,6 +794,11 @@ ipmi::RspType<> ipmiOEMSetProcessorErrConfig(
     std::optional<bool> clearCPUErrorCount,
     std::optional<bool> clearCrashdumpCount, std::optional<uint6_t> reserved3)
 {
+    if (reserved1 || reserved2 || reserved3.value())
+    {
+        return ipmi::responseInvalidFieldRequest();
+    }
+
     std::shared_ptr<sdbusplus::asio::connection> busp = getSdBus();
 
     try
